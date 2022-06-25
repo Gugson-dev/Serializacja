7926119edafd3087a67fbed99bcce83ff4214597
Daniel61538 <d.szypniewski.kontakt@gmail.com>
2022-06-25 11:48:13 +0200

    Add project files.

---
+++ Osoba.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serializacja
+{
+    public class Osoba
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public int Wiek { get; set; }
+        public void Save(string nazwaPliku)
+        {
+            using (var stream = new FileStream(nazwaPliku, FileMode.Create))
+            {
+                var XML = new XmlSerializer(typeof(Osoba));
+                XML.Serialize(stream, this);
+            }
+        }
+        public static Osoba Load(string nazwaPliku)
+        {
+            using (var stream = new FileStream(nazwaPliku, FileMode.Open))
+            {
+                var XML = new XmlSerializer(typeof(Osoba));
+                return (Osoba)XML.Deserialize(stream);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serializacja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba osoba1 = new Osoba();
+            osoba1.Imie = "Daniel";
+            osoba1.Nazwisko = "Szypniewski";
+            osoba1.Wiek = 21;
+            osoba1.Save("DaneOsobowe.xml");
+            Osoba wczytaj = Osoba.Load("DaneOsobowe.xml");
+        }
+    }
+}
